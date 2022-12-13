21f4ab5fd9c7e34bdd5a9c311b7101c93dbfb9c1
MERCY SUCCESS <105282345+MERCYSUCCESS@users.noreply.github.com>
2022-12-13 09:59:03 +0100

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,7 +11,8 @@ export const Projects = () => {
     const [matric, setMatric] = useState<string>("");
     const [selectedFile, setSelectedFile] = useState<string | null >();
 	const [completion, setCompletion] = useState<boolean>(false);
-   
+    const [overlay, setOverlay] = useState<boolean>(false);
+
 
     const fileChangeHandler = (e: any) => {
 		setSelectedFile(e.target.files[0]);
@@ -21,11 +22,17 @@ export const Projects = () => {
     const handleSubmitProject = (e: FormEvent) => {
         e.preventDefault();
 
+        if (name === "" || matric === "") {
+            return toast.error("Fields cannot be empty!", {duration: 3000, id:'empty'});
+        }
+
         if (completion) {
           return toast.error("Portal closed!", {duration: 3000, id:'elapsed'});
         }
 
+
         toast.loading("submitting", {duration: 3000, id:"submit"})
+        setOverlay(true);
 
        setTimeout(() => {
         toast(
@@ -45,12 +52,12 @@ export const Projects = () => {
         setName('');
         setMatric('');
         setSelectedFile(null);
+        setOverlay(false)
        }, 3000)
 
     }
 
 
-    console.log(Date.now());
 
     const [darkMode, setDarkMode] = useDarkMode();
     const toggleDarkMode = (checked: boolean) => {
@@ -59,11 +66,12 @@ export const Projects = () => {
 
     return (
         <section className="h-screen flex items-center flex-col gap-10 justify-center py-12 sm:px-6 lg:px-8">
+            {overlay &&  <div className="bg-black opacity-50 fixed top-0 bottom-0 z-50 w-full h-full left-0 right-0"></div>}
             <CountdownTimer 
                 date={Date.now() + 90000000000}
                 setCompletion={setCompletion}
             />
-            <div className="max-w-md w-full p-3 bg-white dark:bg-slate-800 shadow overflow-hidden sm:rounded-lg space-y-8">
+            <div className="max-w-md w-[95%] p-3 bg-white dark:bg-slate-800 shadow overflow-hidden sm:rounded-lg space-y-8">
                 <div className="flex justify-between">
                     <h1 className="font-medium dark:text-white">Report Submission Portal</h1>
                     <DarkModeSwitch
